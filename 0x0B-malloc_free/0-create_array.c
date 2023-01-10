f5169acb45ba734def353a1dae8e6f5560ecf5c5
CarolineMuiruri <muiruricaroline26@gmail.com>
2023-01-10 07:12:24 -0800

    no imagination

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 		
 #include <stdlib.h>
@@ -22,28 +21,4 @@
 char *create_array(unsigned int size, char c)
 		
 {
-		
-	char *str;
-		
-	unsigned int i;
-		
-
-		
-	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
-
-		
-	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
-		
-}
-
-
 
